8e1858e2a9783872ebf08936858bad795cd9e690
Eric Saou <e.saou@airria.com>
2022-12-30 15:58:49 +0100

    e2e

--- tests/Task/RandomTaskInvalidExpiredAtTest.php
+++ tests/Task/RandomTaskInvalidExpiredAtTest.php
@@ -4,13 +4,12 @@ namespace App\Tests\Task;
 
 use App\Factory\UserFactory;
 use App\Repository\TaskRepository;
-use Facebook\WebDriver\Cookie;
 use Facebook\WebDriver\WebDriverBy;
 use Symfony\Component\Panther\PantherTestCase;
 
-class RandomTaskToggleTest extends PantherTestCase
+class RandomTaskInvalidExpiredAtTest extends PantherTestCase
 {
-    public function testRandomTaskToogleAction() {
+    public function testRandomTaskInvalidExpiredAtAction() {
         $client = self::createPantherClient([
             'browser' => PantherTestCase::CHROME,
         ]);
@@ -24,7 +23,6 @@ class RandomTaskToggleTest extends PantherTestCase
             'username' => $user->getEmail(),
             'password' => "Motdepassergpd1!",
         ]);
-
         $client->submit($form);
         $this->assertSelectorTextContains('.headerTitle', 'Bienvenue sur Todo List');
 
@@ -32,14 +30,23 @@ class RandomTaskToggleTest extends PantherTestCase
         $client->getWebDriver()->findElement(WebDriverBy::linkText('Tâches à faire'))->click();
         $this->assertSelectorTextContains('.headerTitle', 'Tâches à faire');
 
-        // Click sur le bouton "Marquer comme faite" de la première tâche
+        // Click sur le titre de la première tâche pour l'éditer
         $tasks = self::getContainer()->get(TaskRepository::class)->findByRole($user->object());
         $taskId = current($tasks)->getId();
-        $client->waitFor(".taskToggleLink".$taskId);
-        $client->getWebDriver()->findElement(WebDriverBy::className("taskToggleLink".$taskId))->click();
+        $client->waitFor(".taskTitle".$taskId);
+        $client->getWebDriver()->findElement(WebDriverBy::className("taskTitle".$taskId))->click();
 
-        $this->assertSelectorTextContains('.headerTitle', 'Tâches à faire');
-        $this->assertSelectorNotExists(".taskToggleLink".$taskId);
+        $this->assertSelectorTextContains('.updateButton', 'Modifier');
+
+        // Remplissage du formulaire de modification avec une date d'expiration invalide (antérieure)
+        $form = $crawler->selectButton('Modifier')->form([
+            'task[title]' => "Titre d'exemple",
+            'task[content]' => "Contenu d'exemple",
+            'task[expiredAt]' => "2022-03-08 00:00:00",
+        ]);
+
+        $client->submit($form);
+        $this->assertSelectorTextContains('invalid-feedback', 'Cette valeur doit être supérieure');
 
         $client->close();
     }
